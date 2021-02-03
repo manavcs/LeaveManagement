94b0f6cc80e5c74018f60f592052cb49d25e0c90
manvendra <manvendra.yadav@hashedin.com>
2021-02-03 14:22:28 +0530

    initial commit

---
+++ src/test/java/com/manav/LeaveManagement/LeaveManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manav.LeaveManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
