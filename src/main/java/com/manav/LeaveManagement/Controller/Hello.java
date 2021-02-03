246caa342c7837fba0f2a8d271a140addb3b29e0
manvendra <manvendra.yadav@hashedin.com>
2021-02-03 14:51:47 +0530

    added controller

---
+++ src/main/java/com/manav/LeaveManagement/Controller/Hello.java
@@ -0,0 +1,14 @@
+package com.manav.LeaveManagement.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello world";
+    }
+}
